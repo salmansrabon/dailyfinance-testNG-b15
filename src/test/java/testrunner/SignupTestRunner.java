30d68479dbb060503d18335a1ab429db24267ddd
salmansrabon <salmansrabon@gmail.com>
2025-07-09 22:20:10 +0600

    adding user model and save data to json

--- src/test/java/testrunner/SignupTestRunner.java
+++ src/test/java/testrunner/SignupTestRunner.java
@@ -2,8 +2,11 @@ package testrunner;
 
 import com.github.javafaker.Faker;
 import config.Setup;
+import config.UserModel;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 import org.openqa.selenium.By;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import pages.SignupPage;
 import utils.Utils;
@@ -11,7 +14,7 @@ import utils.Utils;
 import java.io.IOException;
 
 public class SignupTestRunner extends Setup {
-    @Test(priority = 1, description = "User can signup with valid data")
+    @Test(priority = 1, description = "User can signup with all data")
     public void doSignup() throws IOException, ParseException {
         driver.findElement(By.partialLinkText("Register")).click();
         SignupPage signupPage=new SignupPage(driver);
@@ -22,8 +25,57 @@ public class SignupTestRunner extends Setup {
         String password="1234";
         String phoneNumber="0130"+ Utils.generateRandomNumber(10000000,99999999);
         String address=faker.address().fullAddress();
-        signupPage.signup(firstName,lastName,email,password,phoneNumber,address);
 
-        Utils.saveUserData(firstName,lastName,email,password,phoneNumber,address);
+        UserModel userModel=new UserModel();
+
+        userModel.setFirstname(firstName);
+        userModel.setLastname(lastName);
+        userModel.setEmail(email);
+        userModel.setPassword(password);
+        userModel.setPhonenumber(phoneNumber);
+        userModel.setAddress(address);
+        signupPage.signup(userModel);
+
+        //save user data
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("firstName",firstName);
+        jsonObject.put("lastName",lastName);
+        jsonObject.put("email",email);
+        jsonObject.put("password",password);
+        jsonObject.put("phoneNumber",phoneNumber);
+        jsonObject.put("address",address);
+
+
+        Utils.saveUserData(jsonObject,"./src/test/resources/users.json");
+    }
+    @Test(priority = 2, description = "User can register without optional data")
+    public void doSignupWithoutOptional() throws IOException, ParseException {
+        SignupPage signupPage=new SignupPage(driver);
+        Faker faker=new Faker();
+        String firstName=faker.name().firstName();
+        String email="salmansrabon+"+ Utils.generateRandomNumber(1000,9999)+"@gmail.com";
+        String password="1234";
+        String phoneNumber="0130"+ Utils.generateRandomNumber(10000000,99999999);
+
+        UserModel userModel=new UserModel();
+
+        userModel.setFirstname(firstName);
+        userModel.setEmail(email);
+        userModel.setPassword(password);
+        userModel.setPhonenumber(phoneNumber);
+        signupPage.signup(userModel);
+
+        //save user data
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("firstName",firstName);
+        jsonObject.put("email",email);
+        jsonObject.put("password",password);
+        jsonObject.put("phoneNumber",phoneNumber);
+
+        Utils.saveUserData(jsonObject,"./src/test/resources/users.json");
+    }
+    @AfterMethod
+    public void navigateToSignupPage(){
+        driver.navigate().to("https://dailyfinance.roadtocareer.net/register");
     }
 }
--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -1,5 +1,6 @@
 package utils;
 
+import config.UserModel;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -15,22 +16,12 @@ public class Utils {
         double randomNumber= Math.random()*(max-min)+min;
         return  (int) randomNumber;
     }
-    public static void saveUserData(String firstName, String lastName, String email, String password, String phoneNumber, String address) throws IOException, ParseException {
-        String url="./src/test/resources/users.json";
+    public static void saveUserData(JSONObject jsonObject, String jsonFilePath) throws IOException, ParseException {
         JSONParser jsonParser=new JSONParser();
-        JSONArray jsonArray= (JSONArray) jsonParser.parse(new FileReader(url));
-
-        JSONObject jsonObject=new JSONObject();
-        jsonObject.put("firstName",firstName);
-        jsonObject.put("lastName",lastName);
-        jsonObject.put("email",email);
-        jsonObject.put("password",password);
-        jsonObject.put("phoneNumber",phoneNumber);
-        jsonObject.put("address",address);
-
+        JSONArray jsonArray= (JSONArray) jsonParser.parse(new FileReader(jsonFilePath));
         jsonArray.add(jsonObject);
 
-        FileWriter fw=new FileWriter(url);
+        FileWriter fw=new FileWriter(jsonFilePath);
         fw.write(jsonArray.toJSONString());
         fw.flush();
         fw.close();
